c7d1080fbef2bf7651f792e27ddcddc759e41a43
lucasbogo <lucasbogo93@gmail.com>
2023-07-20 22:09:37 -0300

    features refactored

--- resources/views/admin/features/feature_view.blade.php
+++ resources/views/admin/features/feature_view.blade.php
@@ -2,7 +2,7 @@
 
 @section('heading')
     <div>
-        <img src="{{ asset('uploads/logo-hotel.png') }}" alt="Logo" alt="Logo" style="padding: 10px;"></a>
+        <img src="{{ asset('uploads/logo-hotel.png') }}" alt="Logo" style="padding: 10px;">
     </div>
     <h3>Ícones Características do Hotel</h3>
 @endsection
@@ -12,47 +12,50 @@
 @endsection
 
 @section('main_content')
-    <div class="section-body">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-bordered" id="example1">
-                                <thead>
-                                    <tr>
-                                        <th>Referencia</th>
-                                        <th>Ícone</th>
-                                        <th>Título</th>
-                                        <th>Ação</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($features as $row)
+    @if ($features->count() > 0)
+        <!-- Check if there are features/icons in the $features array -->
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="example1">
+                                    <thead>
                                         <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>
-                                                <i class="{{ $row->icon }} fz_40"></i>
-                                            </td>
-                                            <td>
-                                                {{ $row->heading }}
-                                            </td>
-                                            <td class="pt_10 pb_10">
-                                                <a href="{{ route('admin_feature_edit', $row->id) }}"
-                                                    class="btn btn-primary">Editar</a>
-                                                <a href="{{ route('admin_feature_delete', $row->id) }}"
-                                                    class="btn btn-danger"
-                                                    onClick="return confirm('Tem Certeza?');">Deletar</a>
-                                            </td>
+                                            <th>Referencia</th>
+                                            <th>Ícone</th>
+                                            <th>Título</th>
+                                            <th>Ação</th>
                                         </tr>
-                                    @endforeach
-                                </tbody>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($features as $row)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>
+                                                    <i class="{{ $row->icon }} fz_40"></i>
+                                                </td>
+                                                <td>
+                                                    {{ $row->heading }}
+                                                </td>
+                                                <td class="pt_10 pb_10">
+                                                    <a href="{{ route('admin_feature_edit', $row->id) }}"
+                                                        class="btn btn-primary">Editar</a>
+                                                    <a href="{{ route('admin_feature_delete', $row->id) }}"
+                                                        class="btn btn-danger"
+                                                        onClick="return confirm('Tem Certeza?');">Deletar</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
 
-                            </table>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endif
 @endsection
