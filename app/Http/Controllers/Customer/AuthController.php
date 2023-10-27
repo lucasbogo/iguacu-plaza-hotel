be3e51693370da0e359d541ff11bd792f106c7f4
lucasbogo <lucasbogo93@gmail.com>
2023-10-27 14:14:18 -0300

    login proper error messages complete

--- app/Http/Controllers/Customer/AuthController.php
+++ app/Http/Controllers/Customer/AuthController.php
@@ -19,10 +19,17 @@ class AuthController extends Controller
 
     public function login_submit(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
+        $request->validate(
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ],
+            [ 
+                'email.required' => 'O campo E-mail é obrigatório.',
+                'email.email' => 'Por favor, insira um endereço válido',
+                'password' => 'O campo Senha é obrigatório'
+            ]
+        );
 
         $credential = [
             'email' => $request->email,
