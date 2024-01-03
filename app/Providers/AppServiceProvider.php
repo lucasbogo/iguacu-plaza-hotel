99bff3006d6a61a2a1d560a264e567e5f67692e2
lucasbogo <lucasbogo93@gmail.com>
2024-01-02 22:52:38 -0300

    deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema; // Import the Schema facade
 use Illuminate\Support\ServiceProvider;
 use App\Models\Page;
 use App\Models\Room;
@@ -25,9 +26,29 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        $page = Page::where('id', 1)->first();
-        $room = Room::where('status', true)->get();
-        $setting = Setting::where('id', 1)->first();
+        // Check if the 'pages' table exists in the database
+        if (Schema::hasTable('pages')) {
+            $page = Page::where('id', 1)->first();
+        } else {
+            // Set default values or handle the absence of the 'pages' table
+            $page = null;
+        }
+
+        // Check if the 'rooms' table exists in the database
+        if (Schema::hasTable('rooms')) {
+            $room = Room::where('status', true)->get();
+        } else {
+            // Set default values or handle the absence of the 'rooms' table
+            $room = null;
+        }
+
+        // Check if the 'settings' table exists in the database
+        if (Schema::hasTable('settings')) {
+            $setting = Setting::where('id', 1)->first();
+        } else {
+            // Set default values or handle the absence of the 'settings' table
+            $setting = null;
+        }
 
         view()->share('global_page', $page);
         view()->share('global_room', $room);
