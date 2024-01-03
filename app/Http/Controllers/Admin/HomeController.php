b99e4fa5d3b05edaf90155c76261e763b8d7d7bc
lucasbogo <lucasbogo93@gmail.com>
2024-01-02 23:29:24 -0300

    test

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -8,19 +8,23 @@ use App\Models\Customer;
 use App\Models\Room;
 use App\Models\Subscriber;
 
-
 class HomeController extends Controller
 {
     public function index()
     {
         $total_completed_orders = Order::where('status', 'Completed')->count();
         $total_pending_orders = Order::where('status', 'Pending')->count();
-        $total_active_customers = Customer::where('status', 1)->count();
+        $total_active_customers = Customer::where('status', 0)->count();
         $total_pending_customers = Customer::where('status', 0)->count();
         $total_rooms = Room::count();
         $total_subscribers = Subscriber::where('status', 1)->count();
 
         $orders = Order::orderBy('id', 'desc')->skip(0)->take(5)->get();
+
+        // If there are no active customers, set a default value
+        if ($total_active_customers === 0) {
+            $total_active_customers = 0;
+        }
 
         return view('admin.home', compact('total_completed_orders', 'total_pending_orders', 'total_active_customers', 'total_pending_customers', 'total_rooms', 'total_subscribers', 'orders'));
     }
