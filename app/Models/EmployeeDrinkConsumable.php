79ad903d73a49aae122ab4783f61724dd9910427
lucasbogo <lucasbogo93@gmail.com>
2024-03-03 18:32:47 -0300

    Model and Migrations Table for Employees and EmployeeDrinkConsumable created

---
+++ app/Models/EmployeeDrinkConsumable.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeeDrinkConsumable extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['employee_id', 'drink_consumable_id', 'quantity', 'price'];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function drinkConsumable()
+    {
+        return $this->belongsTo(DrinkConsumable::class);
+    }
+}
